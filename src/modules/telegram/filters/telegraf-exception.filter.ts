1684fb6e7a875e83aa331ea507c8d60fca252bdf
Aladdin <g.tabroid@gmail.com>
2023-03-22 03:06:53 +0300

    exception filter

--- src/modules/telegram/filters/telegraf-exception.filter.ts
+++ src/modules/telegram/filters/telegraf-exception.filter.ts
@@ -1,14 +1,17 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { TelegrafArgumentsHost } from 'nestjs-telegraf';
 import { Context } from '../interfaces';
 
 @Catch()
 export class TelegrafExceptionFilter implements ExceptionFilter {
+    private logger = new Logger(TelegrafExceptionFilter.name);
+
     async catch(exception: Error, host: ArgumentsHost): Promise<void> {
         const telegrafHost = TelegrafArgumentsHost.create(host);
         const ctx = telegrafHost.getContext<Context>();
         const errorMessage = ctx.$t('errors.base_error', { args: { message: exception.message } });
 
-        await ctx.$replyWithMarkdown(errorMessage);
+        this.logger.error(errorMessage);
+        await ctx.reply(errorMessage);
     }
 }
