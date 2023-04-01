108079a61b9744f31f0c5db6e9b612cc6bc06595
Aladdin <g.tabroid@gmail.com>
2023-04-01 20:15:58 +0300

    Docker config

--- bin/docker.build.bash
+++ bin/docker.build.bash
@@ -1,7 +1,7 @@
 sudo git pull;
-docker rm audio-pack-bot;
-docker rm mpei-audio-pack-bot;
-docker rmi audio-pack-bot;
+docker rm audio-pack-bot -f;
+docker rm mpei-audio-pack-bot -f;
+docker rmi audio-pack-bot -f;
 docker build -t audio-pack-bot ./;
 docker run --env-file ./common/envs/.env --name audio-pack-bot -d audio-pack-bot;
 docker run --env-file ./common/envs/.env.mpei --name mpei-audio-pack-bot -d audio-pack-bot;
